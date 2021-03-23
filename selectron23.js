2d5578f447538d18506403bfe1c83536cbbc33df
realmag777 <realmag777@gmail.com>
2021-03-23 16:09:42 +0100

    Add files via upload
    v.1.0.2

--- selectron23.js
+++ selectron23.js
@@ -1,5 +1,5 @@
 /*!
- * Selectron23 v.1.0.1 // 2021.03.11
+ * Selectron23 v.1.0.2 // 2021.03.23
  * https://github.com/realmag777/selectron23
  *
  * You may use Selectron23 under the terms of the MIT license. Basically that
@@ -8,28 +8,34 @@
  */
 
 'use strict';
-//14-03-2021
+//23-03-2021
 class Selectron23 {
     constructor(element, data = {}) {
-
+        this.data = data;
         Selectron23.z_index = 9999;
 
         this.el = document.createElement('div');
         this.el.className = 'selectron23';
         this.element = element;
-        this.element.insertAdjacentElement('afterend', this.el);
+        if (this.element.tagName.toLowerCase() === 'select') {
+            this.element.insertAdjacentElement('afterend', this.el);
+        } else {
+             this.element.insertAdjacentElement('afterbegin', this.el);
+        }
 
         if (this.element.tagName.toLowerCase() === 'select') {
             //select replacement
             this.element.style.display = 'none';
-            if (this.element.hasAttribute('name')) {
-                //data.name = this.element.getAttribute('name');
+            if (this.element.hasAttribute('style')) {
+                if (this.element.style.width) {
+                    this.data.width = this.element.style.width;
+                }
             }
 
             let selected = null;
             let options = this.element.querySelectorAll('option');
             if (options.length > 0) {
-                let fusion = Boolean(data.fusion);
+                let fusion = Boolean(this.data.fusion);
 
                 let opt = [];
                 options.forEach((o) => {
@@ -56,7 +62,7 @@ class Selectron23 {
 
                 //data fusion
                 if (fusion) {
-                    data.options.forEach((o) => {
+                    this.data.options.forEach((o) => {
                         opt = opt.map((op) => {
                             if (op.value.toString() === o.value.toString()) {
                                 op = o;
@@ -69,10 +75,10 @@ class Selectron23 {
 
                 //+++
 
-                data.options = opt;
+                this.data.options = opt;
 
                 if (selected !== null) {
-                    data.selected = selected;
+                    this.data.selected = selected;
                 }
             }
 
@@ -80,7 +86,7 @@ class Selectron23 {
 
             if (Object.keys(this.element.dataset).length > 0) {
                 Object.keys(this.element.dataset).forEach((key) => {
-                    data[key] = this.element.dataset[key];
+                    this.data[key] = this.element.dataset[key];
                 });
             }
 
@@ -89,7 +95,6 @@ class Selectron23 {
         //+++
 
         this.container = null;
-        this.data = data;
         this.value = null;
         this.input = null;
 
@@ -132,7 +137,8 @@ class Selectron23 {
         }
 
         if (typeof this.data.width !== 'undefined') {
-            this.container.style.width = this.data.width;
+            //this.container.style.width = this.data.width;
+            this.el.style.flexBasis = this.el.style.width = this.data.width;
         }
 
         if (typeof this.data.name !== 'undefined') {
@@ -193,7 +199,7 @@ class Selectron23 {
             if (!text) {
                 margin_top = 'margin-top: -6px;';
             }
-            img = `<img src='${data.img}' alt='' style="float: ${float}; ${margin_top}" />`;
+            img = `<img src='${data.img}' alt='' loading='lazy' style="float: ${float}; ${margin_top}" />`;
         }
 
         option.innerHTML = `${img}<div>${title}${text}</div>`;
@@ -277,40 +283,44 @@ class Selectron23 {
 
     }
 
-    show(is) {
+    show(is, dir = 'down') {
         if (is) {
             if (parseInt(this.container.getAttribute('data-opened'))) {
                 return;
             }
 
-            Selectron23.z_index++;
-            this.container.style.zIndex = Selectron23.z_index;//to avoid css issue with anothers selectron23 drop-downs
+            this.container.style.zIndex = ++Selectron23.z_index;//to avoid css issue with anothers selectron23 drop-downs
 
-            if (this.container.querySelectorAll('.selectron23-option').length <= 2) {
-                this.container.style.maxHeight = '100%';
-                this.container.setAttribute('data-opened', 1);
-                this.pointer.style.top = (parseInt(this.pointer.style.top) + 3) + 'px';
-            } else {
-                let counter = 1;
-                //animation
-                let timer = setInterval(() => {
-                    let max_height = 0;
+            //animation
+            let counter = 1;
+            let timer = setInterval(() => {
+                let max_height = 0;
+
+                if (typeof this.data.max_open_height !== 'undefined') {
+                    max_height = parseInt(this.data.max_open_height);
+                } else {
                     this.container.querySelectorAll('.selectron23-option').forEach(function (item) {
                         max_height += item.offsetHeight;
                     });
+                }
 
-                    //growing
-                    this.container.style.maxHeight = parseFloat(0.05 * counter) * max_height + 'px';
 
-                    if (parseInt(this.container.style.maxHeight) >= max_height) {
-                        clearInterval(timer);
-                        this.container.style.maxHeight = '100%';
-                        this.container.setAttribute('data-opened', 1);
-                        this.pointer.style.top = (parseInt(this.pointer.style.top) + 3) + 'px';
+                //growing
+                this.container.style.maxHeight = parseFloat(0.05 * counter) * max_height + 'px';
+
+                if (parseInt(this.container.style.maxHeight) >= max_height) {
+                    clearInterval(timer);
+                    if (typeof this.data.max_open_height !== 'undefined') {
+                        this.container.style.maxHeight = max_height + 'px';
+                        this.container.style.overflow = 'auto';
+                    } else {
+                        this.container.style.maxHeight = '100vh';
                     }
-                    counter++;
-                }, 10);
-            }
+                    this.container.setAttribute('data-opened', 1);
+                    this.pointer.style.top = (parseInt(this.pointer.style.top) + 3) + 'px';
+                }
+                counter++;
+            }, 10);
         } else {
             if (!parseInt(this.container.getAttribute('data-opened'))) {
                 return;
@@ -321,7 +331,11 @@ class Selectron23 {
 
             this._normalize_min_height();
             this.container.style.maxHeight = this.container.style.minHeight;
-        }
+            if (typeof this.container.style.overflow !== 'undefined') {
+                this.container.scrollTop = 0;//!!
+                this.container.style.overflow = null;
+            }
+    }
     }
 
     _normalize_min_height() {
